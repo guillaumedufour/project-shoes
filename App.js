35f17b9a0aedf9bea9e2f7f3ff327d9dc54e0d84
Guillaume Gdlab <guillaume@gdlab.fr>
2025-11-08 16:33:48 +0100

    App.js: remove unused import + style

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import {StatusBar} from "expo-status-bar";
-import {StyleSheet, Text, View} from "react-native";
 import {useFonts} from "expo-font"
 import HomeScreen from "./screens/home";
 
@@ -13,12 +11,3 @@ export default function App() {
 
   return fontsLoaded ? <HomeScreen/> : null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
